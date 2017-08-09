36db7cfd1afca0e136cbe3424df89a6cd0eaa492
ved <prakashh108@gmail.com>
2017-08-10 01:48:44 +0530

    Merged to master

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const Assert = require('assert');
+const ReactTestUtils = require('react-addons-test-utils');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+
